6199321a7c2e17f17c02421cc7f6b818f90410b4
Artemiy Pavlychev <linkartemiy@gmail.com>
2023-03-09 00:32:02 +0300

    feat: включена xml документация

--- Poolit/Models/User.cs
+++ Poolit/Models/User.cs
@@ -1,10 +1,9 @@
 ﻿namespace Poolit.Models;
 
-[Serializable]
 public class User
 {
     public int Id { get; set; }
-    public required string Login { get; set; }
+    public string Login { get; set; }
     public string HashedPassword { get; set; }
     public string Token { get; set; }
 }
